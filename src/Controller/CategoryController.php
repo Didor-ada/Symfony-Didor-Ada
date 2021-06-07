65db141e36d65d611c3f59827c42f89ad2c34105
Didor <didor.dada@gmail.com>
2021-06-07 21:24:59 +0200

    quete12finie

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,7 +6,9 @@ use App\Entity\Category;
 use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CategoryType;
 
 /**
  * @Route("/category", name="category_")
@@ -29,6 +31,26 @@ class CategoryController extends AbstractController
                 ['categories' => $categories]
             );
         }
+    }
+
+    /**
+     * The controller for the category add form
+     *
+     * @Route("/new", name="new")
+     */
+
+    public function new(Request $request) : Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('category_index');
+        }
+        return $this->render('category/new.html.twig', ["form" => $form->createView()]);
     }
 
     /**
